cd9d280cc4802cf0243bb737fa174e789fd5e6ba
Raju Subba <rajusubba14@gmail.com>
2026-01-06 17:28:22 -0500

    fix the application.py

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ def home():
 @app.route("/predict", methods=["GET", "POST"])
 def predict_datapoint():
     if request.method == "GET":
-        return "Use POST to /predict. Go to / to use the form."
+        return render_template("home.html")
 
     # ✅ Read values from the form
     reading_score = request.form.get("reading_score")
@@ -26,7 +26,7 @@ def predict_datapoint():
 
     data = CustomData(
         gender=request.form.get("gender"),
-        race_ethnicity=request.form.get("race_ethnicity"),
+        race_ethnicity=request.form.get("ethnicity"),
         parental_level_of_education=request.form.get("parental_level_of_education"),
         lunch=request.form.get("lunch"),
         test_preparation_course=request.form.get("test_preparation_course"),
